5c9e9f4c5d1464f9db0bef30a605a0d2bfc8cecb
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2015-07-29 23:28:28 +0200

    initialize form and handle input

--- lib/MUVideo/Form/Handler/Collection/GetVideos.php
+++ lib/MUVideo/Form/Handler/Collection/GetVideos.php
@@ -17,5 +17,60 @@
  */
 class MUVideo_Form_Handler_Collection_GetVideos extends MUVideo_Form_Handler_Collection_Base_GetVideos
 {
-    // feel free to extend the base handler class here
+    /**
+     * Post-initialise hook.
+     *
+     * @return void
+     */
+    public function postInitialize()
+    {
+        $playLists = ModUtil::getVar($this->name, 'channelIds');
+        $playLists = explode(';', $playLists);
+        if (count($playLists) == 1) {
+            $playLists = explode(',', $playLists[0]);         
+            // initialise list entries for the 'channelId' setting
+            $formVars['channelIdItems'] = array(array('value' => $playLists[0], 'text' => $playLists[1]));
+            // assign all module vars
+            $this->view->assign('getVideos', $formVars);
+        } else {
+            foreach ($playLists as $playList) {
+                $thisPlaylist = explode(',', $playList);
+                $formVars['channelIdItems'][] = array('value' => $thisPlayList[0], 'text' => $thisPlaylist[1]);
+            }
+            $this->view->assign('getVideos', $formVars);
+        }
+        parent::postInitialize();
+    }
+    
+    /**
+     * Command event handler.
+     *
+     * This event handler is called when a command is issued by the user.
+     *
+     * @param Zikula_Form_View $view The form view instance.
+     * @param array            $args Additional arguments.
+     *
+     * @return mixed Redirect or false on errors.
+     */
+    public function handleCommand(Zikula_Form_View $view, &$args)
+    {
+        // get collection id
+        
+        $collectionId = $this->request->query->filter('collectionId', 0, FILTER_SANITIZE_NUMBER_INT);
+        
+        if ($collectionId == 0) {
+            return LogUtil::registerError(__('Sorry. There is no valid collection id!'));
+        }
+
+        // get channel id from form
+        $channelId = $this->request->request->filter('channelId', '', FILTER_SANITIZE_STRING);
+        
+        $serviceManager = ServiceUtil::getManager();
+        $controllerHelper = new MUVideo_Util_Controller($serviceManager);
+        
+        return $controllerHelper->getYoutubeVideos($channelId[0]);
+    
+       // return $this->view->redirect($this->getRedirectUrl($args));
+    }
+    
 }
