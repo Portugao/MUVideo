1c424b3c1564f89385e6dbcf7e5bde832e790b49
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2015-07-29 23:28:07 +0200

    first code to create new movie

--- lib/MUVideo/Util/Controller.php
+++ lib/MUVideo/Util/Controller.php
@@ -17,19 +17,36 @@
 class MUVideo_Util_Controller extends MUVideo_Util_Base_Controller
 {
     /*
-     * 
+     *
      * this function is to get youtube videos into MUVideo
-     * 
+     *
      */
-    public function getYoutubeVideos($youtubeApi)
-    {          
-        $api = get_data("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={DEINE_PLAYLIST_ID}&maxResults=10&fields=items%2Fsnippet&key=" . $youtubeApi);
-        
-        $video = json_decode($api, true);
+    public function getYoutubeVideos($channelId = '')
+    {
+        $youtubeApi = ModUtil::getVar($this->name, 'youtubeApi');
+
+        $api = self::getData("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" . $channelId  . "&key=" . $youtubeApi);
+        // https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCJC8ynLpY_q89tmNhqIf1Sg&key={YOUR_API_KEY}
+        //$api = self::getData("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={DEINE_PLAYLIST_ID}&maxResults=10&fields=items%2Fsnippet&key=" . $youtubeApi);
+
+        $videos = json_decode($api, true);
+
+        foreach ($videos['items'] as $videoData) {
+            if (isset($videoData['id']['videoId'])) {
+                LogUtil::registerError($videoData['id']['videoId']);
+                 
+                $newYoutubeVideo = new MUVideo_Entity_Movie();
+                $newYoutubeVideo->setTitle($videoData['snippet']['title']);
+                $newYoutubeVideo->setDescription($videoData['snippet']['description']);
+                $newYoutubeVideo->setUrlOfYoutube('https://www.youtube.com/watch?v=' . $videoData['id']['videoId']);
+                
+                
+            }
+        }
     }
-    
+
     /*
-     * 
+     *
      * this function is to call a url, for example a youtube call
      */
     public function getData($url)
