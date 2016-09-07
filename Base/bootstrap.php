69f48a182b524ede3d7918ee69dc978dcc402fc2
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2016-09-07 17:13:36 +0200

    new bootstrap

--- Base/bootstrap.php
+++ Base/bootstrap.php
@@ -21,4 +21,15 @@
 $helper = ServiceUtil::getService('doctrine_extensions');
 $helper->getListener('timestampable');
 $helper->getListener('standardfields');
+$translatableListener = $helper->getListener('translatable');
+//$translatableListener->setTranslatableLocale(ZLanguage::getLanguageCode());
+$currentLanguage = preg_replace('#[^a-z-].#', '', FormUtil::getPassedValue('lang', System::getVar('language_i18n', 'en'), 'GET'));
+$translatableListener->setTranslatableLocale($currentLanguage);
+/*
+ * Sometimes it is desired to set a default translation as a fallback if record does not have a translation
+ * on used locale. In that case Translation Listener takes the current value of Entity.
+ * But there is a way to specify a default locale which would force Entity to not update it`s field
+ * if current locale is not a default.
+ */
+//$translatableListener->setDefaultLocale(System::getVar('language_i18n', 'en'));
 
