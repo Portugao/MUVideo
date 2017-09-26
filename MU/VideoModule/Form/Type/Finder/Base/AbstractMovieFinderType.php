497ffcc46f620f503da69674505b8a40ea7fed38
Portugao <info@homepages-mit-zikula.de>
2017-09-26 08:48:05 +0200

    more editors

--- MU/VideoModule/Form/Type/Finder/Base/AbstractMovieFinderType.php
+++ MU/VideoModule/Form/Type/Finder/Base/AbstractMovieFinderType.php
@@ -281,7 +281,7 @@ abstract class AbstractMovieFinderType extends AbstractType
             ->setRequired(['object_type', 'editor_name'])
             ->setAllowedTypes('object_type', 'string')
             ->setAllowedTypes('editor_name', 'string')
-            ->setAllowedValues('editor_name', ['tinymce', 'ckeditor'])
+            ->setAllowedValues('editor_name', ['ckeditor', 'quill', 'summernote', 'tinymce'])
         ;
     }
 }
