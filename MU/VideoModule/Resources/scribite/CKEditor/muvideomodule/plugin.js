1704dd58af9f23276dc2a90e10d30f3033b0862e
Portugao <info@homepages-mit-zikula.de>
2017-09-26 09:18:40 +0200

    scribite plugin update

--- MU/VideoModule/Resources/scribite/CKEditor/muvideomodule/plugin.js
+++ MU/VideoModule/Resources/scribite/CKEditor/muvideomodule/plugin.js
@@ -1,18 +1,15 @@
 CKEDITOR.plugins.add('muvideomodule', {
     requires: 'popup',
-    lang: 'en,nl,de',
     init: function (editor) {
         editor.addCommand('insertMUVideoModule', {
             exec: function (editor) {
-                var url = Routing.generate('muvideomodule_external_finder', { objectType: 'collection', editor: 'ckeditor' });
-                // call method in MUVideoModule.Finder.js and provide current editor
-                MUVideoModuleFinderCKEditor(editor, url);
+                MUVideoModuleFinderOpenPopup(editor, 'ckeditor');
             }
         });
         editor.ui.addButton('muvideomodule', {
-            label: editor.lang.muvideomodule.title,
+            label: 'Video',
             command: 'insertMUVideoModule',
-            icon: this.path.replace('scribite/CKEditor/muvideomodule', 'public/images') + 'admin.png'
+            icon: this.path.replace('scribite/CKEditor/muvideomodule', 'images') + 'admin.png'
         });
     }
 });
