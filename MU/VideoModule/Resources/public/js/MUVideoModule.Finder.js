730156020afc6570ceaeecf7b2ba88383b3587cb
Portugao <info@homepages-mit-zikula.de>
2017-11-20 20:15:17 +0100

    typo

--- MU/VideoModule/Resources/public/js/MUVideoModule.Finder.js
+++ MU/VideoModule/Resources/public/js/MUVideoModule.Finder.js
@@ -115,25 +115,25 @@ function mUVideoGetPasteSnippet(mode, itemId)
     imagePath = jQuery('#imagePath' + itemId).length > 0 ? jQuery('#imagePath' + itemId).val().replace(quoteFinder, '') : '';
     pasteMode = jQuery("[id$='pasteAs']").first().val();
 
-    // embed video
+ // embed video
     if (pasteMode === '3') {
-        return 'YOUTUBE[' + itemId + ']';;
+        return 'YOUTUBE[' + itemId + ']';
     }
-    
+
     // embed playlist
     if (pasteMode === '4') {
-        return 'PLAYLIST[' + itemId + ']';;
+        return 'PLAYLIST[' + itemId + ']';
     }
-    
+
     // embed video
     if (pasteMode === '10') {
-        return 'YOUTUBED[' + itemId + ']';;
+        return 'YOUTUBED[' + itemId + ']';
     }
     // embed playlist
     if (pasteMode === '11') {
-        return 'PLAYLISTD[' + itemId + ']';;
+        return 'PLAYLISTD[' + itemId + ']';
     }
-
+    
     // relative link to detail page
     if (pasteMode === '1') {
         return mode === 'url' ? itemPath : '<a href="' + itemPath + '" title="' + itemDescription + '">' + itemTitle + '</a>';
